777c02fa9d59ab9cf1836632da3746d230eaf1e9
INTERNAL\tcooper <tcooper@cuanswers.com>
2017-07-26 17:49:09 -0400

    [master]: Start building beer rest service

--- BeerWithRest/Controllers/BeerController.cs
+++ BeerWithRest/Controllers/BeerController.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using BeerWithRest.Models;
+using BeerWithRest.Services;
 
 namespace BeerWithRest.Controllers
 {
     public class BeerController : ApiController
     {
+        private BeerRepository beerRepository;
+
+        public BeerController()
+        {
+            this.beerRepository = new BeerRepository();
+        }
+
         public Beer[] Get()
         {
-            return new Beer[]
-            {
-                new Beer
-                {
-                    Id = 1,
-                    Name = "Red's Rye"
-                },
-                new Beer
-                {
-                    Id = 2,
-                    Name = "Dragon's Milk"
-                }
-            };
+            return beerRepository.GetAllBeers();
         }
     }
 }
---
+++ BeerWithRest/Services/BeerRepository.cs
@@ -0,0 +1,25 @@
+﻿using BeerWithRest.Models;
+
+namespace BeerWithRest.Services
+{
+    public class BeerRepository
+    {
+        public Beer[] GetAllBeers()
+        {
+            return new Beer[]
+            {
+                new Beer
+                {
+                    Id = 1,
+                    Name = "Red's Rye"
+                },
+                new Beer
+                {
+                    Id = 2,
+                    Name = "Dragon's Milk"
+                }
+            };
+
+        }
+    }
+}
